68c617e6b6b0f17c73d04946618aa726f1f026c4
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2022-11-22 15:07:54 +0100

    Update manta-web-app-scraper.py

--- manta-web-app-scraper.py
+++ manta-web-app-scraper.py
@@ -30,7 +30,7 @@ _ = installff()
 
 def scrape(category, city, state, pages):
     st.text('Loading and downloading the driver')
-    driver = start_chrome(capabilities=caps, options=options)
+    driver = uc.Chrome(desired_capabilities=caps, options=options)
     st.text('Browser loaded')
 
     items_list = []
